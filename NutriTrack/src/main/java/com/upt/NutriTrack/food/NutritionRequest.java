8009803c0ccff620593bdae541bdc19894730d6c
Klevis-Pllumbi <pllumbiklevis1@gmail.com>
2025-01-23 07:53:17 +0100

    added the classes for food needed for further functionalities

---
+++ NutriTrack/src/main/java/com/upt/NutriTrack/food/NutritionRequest.java
@@ -0,0 +1,44 @@
+package com.upt.NutriTrack.food;
+
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal;
+
+public class NutritionRequest {
+
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Min(0)
+    private Integer calorieCount;
+
+    @NotNull
+    @DecimalMin("0.00")
+    @Digits(integer = 8, fraction = 2)
+    private BigDecimal cost;
+
+    // Getters and Setters
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Integer getCalorieCount() {
+        return calorieCount;
+    }
+
+    public void setCalorieCount(Integer calorieCount) {
+        this.calorieCount = calorieCount;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+}
